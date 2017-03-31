33469abdeba450e43cdb4d6295b39f8fa8c508fd
José Gutiérrez <guti7@users.noreply.github.com>
2017-03-31 02:41:33 -0700

    Implement queue with linked list.

--- source/queue.py
+++ source/queue.py
@@ -6,7 +6,7 @@ from linkedlist import LinkedList
 # implement LinkedQueue below, then change the assignment at the bottom
 # to use this Queue implementation to verify it passes all tests
 class LinkedQueue(object):
-
+    # FIFO
     def __init__(self, iterable=None):
         """Initialize this queue and enqueue the given items, if any"""
         # Initialize a new linked list to store the items
@@ -17,35 +17,37 @@ class LinkedQueue(object):
 
     def __repr__(self):
         """Return a string representation of this queue"""
-        return 'Queue({} items, top={})'.format(self.length(), self.front())
+        return 'Queue({} items, front={})'.format(self.length(), self.front())
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise"""
-        # TODO: Check if empty
-        pass
+        # Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this queue"""
-        # TODO: Count number of items
-        pass
+        # Count number of items
+        return self.list.length()
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue"""
-        # TODO: Insert given item
-        pass
+        # Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty"""
-        # TODO: Return front item, if any
-        pass
+        # Return front item, if any
+        return self.list.get_at_index(0) if not self.is_empty() else None
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty"""
         # TODO: Remove and return front item, if any
-        pass
-
+        front = self.front()
+        self.list.delete(front)
+        return front
+        
 
 # implement ArrayQueue below, then change the assignment at the bottom
 # to use this Queue implementation to verify it passes all tests
@@ -97,5 +99,5 @@ class ArrayQueue(object):
 
 # implement LinkedQueue and ArrayQueue above, then change the assignment below
 # to use each of your Queue implementations to verify they each pass all tests
-# Queue = LinkedQueue
-Queue = ArrayQueue
+Queue = LinkedQueue
+# Queue = ArrayQueue
