9832dd4ccad89b10412b003be974badb38c5266f
José Gutiérrez <guti7@users.noreply.github.com>
2017-04-14 11:41:33 -0700

    counting sort with iteration.

--- source/sortingalgorithms.py
+++ source/sortingalgorithms.py
@@ -42,15 +42,57 @@ def tree_sort(items):
     tree = BinarySearchTree(items)
     return tree.items_in_order()
 
+def counting_sort(items):
+    length = len(items)
+
+    # Count the number of distinct keys(items) in a histogram
+    k = max(items) + 1
+    count = [0 for _ in range(k)]
+
+    for i in range(length):  # 0 to
+        value = items[i]
+        count[value] += 1
+    print('Counts: {}'.format(count))
+
+    # Sum up counts to calculate the actual ordered position for each key
+    for i in range(1, k):
+        count[i] += count[i - 1]
+    print('Sum: {}'.format(count))
+
+    output = [0 for _ in items]
+    for i in range(length):
+        count_index = items[i]
+        position = count[count_index] - 1
+        output[position] = items[i]
+        count[count_index] -= 1
+
+    return output
+
+def human_sort(orig):
+    source = orig[:]  # create shallow copy
+    sorted_items = []       # init sorted array
+    source_len = len(source)
+    for _ in range(source_len):         # repeat for every elem
+        min_idx = 0
+        for j in range(len(source)):    # find min in source
+            compare = source[min_idx]   # elem to compare against
+            current = source[j]         # current elem in iteration
+            if current < compare:
+                min_idx = j
+        sorted_items.append(source[min_idx])  # add min to sorted
+        del source[min_idx]             # remove from source
+    return sorted_items
 
 def test_sorting():
-    array = [2, 4, 3, 1, 5]
+    array = [2, 1, 5, 2, 2, 1, 4, 0]
     print(array)
     # print(bubble_sort(array))
     # assert bubble_sort(array) == [1, 2, 3, 4, 5]
     # print(selection_sort(array))
     # print(insertion_sort(array))
-    print(tree_sort(array))
+    # print(tree_sort(array))
+    # print(human_sort(array))
+    print('test: {}.'.format(counting_sort(array)))
 
 if __name__ == '__main__':
     test_sorting()
